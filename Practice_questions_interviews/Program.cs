5b859c6178dad63ea8cfe913ada9843aa7878928
Tyson Nguyen <yngu4822@gmail.com>
2023-07-02 14:50:44 +1000

    first commit into github

---
+++ Practice_questions_interviews/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Practice_questions_interviews
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
