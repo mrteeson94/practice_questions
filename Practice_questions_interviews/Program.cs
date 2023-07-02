f831b8f4038bd4a2d2fed4c8fb7147948f4601f8
Tyson Nguyen <yngu4822@gmail.com>
2023-07-02 15:07:11 +1000

    string manipulation questions added

--- Practice_questions_interviews/Program.cs
+++ Practice_questions_interviews/Program.cs
@@ -6,7 +6,19 @@ namespace Practice_questions_interviews
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //** Basic string manipulation **
+            //Create app asking for user full name, age 
+            //output: valid input message, fullname, firstname, lastname, age and recommended fullname nickname(replace given fullname with reversed version)
+
+            //i. input
+            Console.Write("What is your fullname?: ");
+            var fullName = Console.ReadLine();
+            Console.WriteLine(fullName);
+
+            fullName = fullName.Trim();
+            var firstName = 
+            Console.WriteLine(fullName);
+
         }
     }
 }
